2eb4485bc8623aa08e4449b5807e90e700a227fe
SivaKrishna6745 <krishnasiva6745@gmail.com>
2022-09-13 20:31:12 +0530

    day 03 exercise 01 file

---
+++ solutions/day-03/exercise1.js
@@ -0,0 +1,29 @@
+// What is a module?
+// A module is a single or multiple functions that can be exported into the project when needed.
+
+// What is package?
+// A package is a module or collection of modules. Eg: React, ReactDOM, etc
+
+// What is the difference between a module and a package.
+// A Package is a collection of modules either it's one or more
+
+// What is NPM?
+// NPM is Node Package Manager. It is a default package of nodeJS.
+
+// What is Webpack?
+// Webpack is a free, open-source module bundler for JavaScript.
+
+// How do you create a new React project?
+// using npx create-react-app
+
+// What are the files and folders inside a project folder(package.json, package-lock.json or yarn.lock, .gitignore,node_modules and public)?
+// node_modules, public, src, package.json, package-lock.json
+
+// What is your favorite code editor (I believe that it is Visual Studio Code)?
+// No doubt, it is Visual Studio Code
+
+// Add different Visual Studio Code extensions to improve your productivity(eg. prettier, ESLint etc).
+// prettier, brackett pair colorizer, babel, auto close tag
+
+// Try to make a different custom module in a different file and import it to index.js.
+import { addTwo } from "./custom-module";
